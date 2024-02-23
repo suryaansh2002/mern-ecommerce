9d8253652d68e2efcf101018f633e8f0ce8b5baa
suryaansh2002 <suryaansh2002@gmail.com>
2024-02-23 07:56:08 +0530

    Nodeenv updated

--- server/index.js
+++ server/index.js
@@ -55,14 +55,14 @@ app.use('/api', braintreeRoutes);
 app.use('/api', orderRoutes);
 
 // // Server static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-//   // Set static folder
-//   app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.get("/", function (req, res) {
   res.send("Welcome to backend");
 });
