78d9192d0cc074ca6c58f5122db70800268b25e6
suryaansh2002 <suryaansh2002@gmail.com>
2024-02-23 07:30:40 +0530

    Redeploying 2

--- server/index.js
+++ server/index.js
@@ -54,15 +54,15 @@ app.use('/api', productRoutes);
 app.use('/api', braintreeRoutes);
 app.use('/api', orderRoutes);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// // Server static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 app.get("/", function (req, res) {
   res.send("Welcome to backend");
 });
